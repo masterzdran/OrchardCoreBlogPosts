716c05f0873e414b451fa776f6642849f58fbc0b
Nuno Cancelo <nuno.cancelo@gmail.com>
2020-09-13 15:31:59 +0100

    Orchard Core Blog Posts Demos

---
+++ OrchardCoreTheme.Freelancer/Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "Freelancer",
+    Author = "Nuno Cancelo",
+    Website = "http://masterzdran.github.io/",
+    Version = "1.0.0",
+    Description = "Freelance based theme for Orchard Core."
+)]
